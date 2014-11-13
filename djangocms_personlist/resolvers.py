556b64be4a5f7bc76a078624b6aa3599c4190b5c
Christian Kohout <ck@getaweb.at>
2014-11-13 12:29:34 +0100

    added prefix fallback

--- djangocms_personlist/resolvers.py
+++ djangocms_personlist/resolvers.py
@@ -1,8 +1,11 @@
 from django.core.urlresolvers import resolve, reverse as _reverse
+from django.conf import settings
 
 
 def reverse(request, prefix, app_name, view_name, kwargs={}):
     _namespace = resolve(request.path).namespace
-    if prefix is not None and app_name is not None:
+    if not prefix:
+        prefix = settings.SITE_PREFIX
+    if app_name is not None:
         _namespace = prefix + '-' + app_name
     return _reverse(u':'.join([_namespace, view_name]), kwargs=kwargs)
